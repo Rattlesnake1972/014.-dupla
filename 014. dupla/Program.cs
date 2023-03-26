cd65ec98459cc25ef34711f05a4c38f7f1aa2370
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 14:36:45 +0200

    Bekér egy számot a és megduplázza azt

---
+++ 014. dupla/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014.dupla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot és kiírom a dupláját! ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(2 * szam);
+
+            Console.ReadKey();
+        }
+    }
+}
